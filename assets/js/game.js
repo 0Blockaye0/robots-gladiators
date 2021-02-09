6d500c732c59ca032cfbb39ff80a7812cd5d56e6
_blockaye_ <blake_austinjohnson@icloud.com>
2021-02-09 12:42:49 -0600

    added multiple enemies and created for loop to battle each one

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,13 +4,13 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 // You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
+//console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Andriod", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function () {
+var fight = function (enemyName) {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
   // Asks if they want to skip or fight
@@ -65,5 +65,29 @@ var fight = function () {
   }
 
 };
+// run fight function to start game
+//fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
 
-fight();
+
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
+console.log(enemyNames)
+
+console.log(enemyNames[0])
+console.log(enemyNames[1])
+console.log(enemyNames[2])
+console.log(enemyNames[3])
+console.log(enemyNames.length)
+
+for(var i = 0; i < enemyNames.length; i++) {
+  console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyNames[i] + " is at " + i + " index");
+}
